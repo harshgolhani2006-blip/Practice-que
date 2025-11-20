0c16265d94714a6611ff44299d91b9d3315207a9
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2025-11-20 23:37:19 +0530

    Forty Day

---
+++ Forty Day/forty_day.c
@@ -0,0 +1,57 @@
+// WAP TO MATRIX MULTIPICATION
+#include <stdio.h>
+
+int main() {
+
+    int a[10][10], b[10][10], c[10][10];
+    int r1, c1, r2, c2;
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    // Check condition
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible!");
+        return 0;
+    }
+
+    // Input first matrix
+    printf("Enter element of first matrix:\n");
+    for(int i = 0; i < r1; i++) {
+        for(int j = 0; j < c1; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter element of second matrix:\n");
+    for(int i = 0; i < r2; i++) {
+        for(int j = 0; j < c2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Multiplying matrices
+    for(int i = 0; i < r1; i++) {
+        for(int j = 0; j < c2; j++) {
+            c[i][j] = 0;
+            for(int k = 0; k < c1; k++) {
+                c[i][j] += a[i][k] * b[k][j];  
+            }
+        }
+    }
+
+    // Output resultant matrix
+    printf("Resultant matrix is:\n");
+    for(int i = 0; i < r1; i++) {
+        for(int j = 0; j < c2; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
